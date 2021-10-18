d4d2bcbb02ac268c190307cc7e86927524908b8f
TheJis <ing.jesithgarcia@gmail.com>
2021-10-18 10:15:51 -0500

    React forms with Formik Yup and Material UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { useFormik } from 'formik'
+import * as yup from 'yup'
+import { TextField, Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core'
+
+const validationSchema = yup.object({
+  firstName: yup.string().required("First Name is required"),
+  lastName: yup.string().required("Last Name is required"),
+  email: yup.string().email("Enter a valid Email").required("Email is required")
+})
+
 
 function App() {
+  const classes = useStyle()
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: ""
+    },
+    onSubmit: (values) => {
+      console.log(JSON.stringify(values))
+    },
+    validationSchema: validationSchema
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form className={classes.form} onSubmit={formik.handleSubmit}>
+        <TextField 
+          id="firstName" name="firstName" 
+          label="First Name" margin="normal" 
+          value={formik.values.firstName}
+          onChange={formik.handleChange}
+          error={formik.touched.firstName && Boolean(formik.errors.firstName)}
+          helperText={formik.touched.firstName && formik.errors.firstName}
+        />
+        <TextField 
+          id="lastName" name="lastName" 
+          label="Last Name" margin="normal"
+          value={formik.values.lastName}
+          onChange={formik.handleChange}
+          error={formik.touched.lastName && Boolean(formik.errors.lastName)}
+          helperText={formik.touched.lastName && formik.errors.lastName}
+         />
+        <TextField 
+          id="email" name="email" 
+          label="Email Address" margin="normal"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+        <Button type="submit" variant="outlined">submit</Button> 
+      </form>
+    </>
   );
 }
 
+const useStyle = makeStyles({
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '20rem',
+    margin: '5rem auto'
+  }
+})
+
 export default App;
+
